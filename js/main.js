cd658bac7ef4423d97a2ec6a173b376e6bae6aed
frontenddev344 <frontenddev344@gmail.com>
2025-07-18 14:59:31 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -122,3 +122,31 @@ $(document).ready(function () {
         $(this).slick('slickPlay');
     });
 });
+
+
+  $(document).ready(function () {
+    $('.responsive-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      speed: 600,
+      arrows: true,
+      dots: false,
+      prevArrow: $('#prevBtn'),
+      nextArrow: $('#nextBtn'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+      ]
+    });
+  });
